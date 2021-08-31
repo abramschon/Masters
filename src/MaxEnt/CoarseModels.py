ae4b804d03ee8264b9c94362b72466f7fe0565ae
Abram Schonfeldt <abram.schonfeldt@gmail.com>
2021-08-31 14:55:45 +0200

    Creates sampling for ind and K pop models

--- src/MaxEnt/CoarseModels.py
+++ src/MaxEnt/CoarseModels.py
@@ -1,10 +1,13 @@
 import numpy as np
 from math import comb
+from numba import njit
 from .utils import get_state_space
+from .Samplers import gibbs_sampling
 
 class Independent:
     """
     Represents an independent or factorised model
+    If sampling is True, all estimates will be based off of samples and the state space will not be set up
     Variables:
         N - no. spins
         avgs - vector of expectations for each spin
@@ -13,16 +16,20 @@ class Independent:
         h - vector of the local magnetic field
         Z - the partition function
     """
-    def __init__(self, N, avgs, spin_vals=[0,1]):
+    def __init__(self, N, avgs, spin_vals=[0,1], sampling=False):
         # set user input
+        self.sampling = sampling 
         self.N = N
         self.avgs = avgs
         self.spin_vals = spin_vals
-        # determine all states
-        self.states = get_state_space(N, spin_vals, dtype=np.byte)
-        # randomly initialise h and J
+        # initialise h and Z
         self.h = - np.log(avgs/(1-avgs))
         self.Z = np.prod(1+np.exp(-self.h))
+        #possibly init state space 
+        self.states = None
+        if not sampling: # determine all states
+            self.states = get_state_space(N, spin_vals, dtype=np.byte)
+        
     
     # Methods for calculating probabilities and expectations over entire distribution
     def expectation(self, f):
@@ -38,12 +45,6 @@ class Independent:
         Returns a vector of the expected values <s_i>
         """
         return self.expectation(lambda states: states)
-    
-    def correlations(self):
-        """
-        Returns a matrix of the expected values <s_i s_j> where i != j
-        """
-        return np.triu( self.states.T@np.diag(self.p(self.states))@self.states, 1)
 
     def p(self, s):
         """
@@ -61,6 +62,15 @@ class Independent:
             s - np.array of the state/states
         """
         return s@self.h 
+    
+    def get_samples(self, M=100000, chains = 4):
+        h = self.h
+        @njit
+        def p(s):
+            return np.exp(-h.dot(s))
+        init_states = np.array([np.random.binomial(1,self.avgs) for c in range(chains)])
+        return gibbs_sampling(p,init_states,M,avg_every=self.N,burn_in=0)
+        
 
 class PopCount:
     """
@@ -73,15 +83,17 @@ class PopCount:
         h - vector of the local magnetic field
         Z - the partition function
     """
-    def __init__(self, N, p_K, spin_vals=[0,1]):
+    def __init__(self, N, p_K, spin_vals=[0,1], sampling=False):
         # set user input
         self.N = N
         self.p_K = p_K
         self.spin_vals = spin_vals
-        # determine all states
-        self.states = get_state_space(N, spin_vals, dtype=np.byte)
         # get all combinations of N choose K
         self.combs = np.array([comb(N,K) for K in np.arange(N+1)])
+        #possibly init state space 
+        self.states = None
+        if not sampling: # determine all states
+            self.states = get_state_space(N, spin_vals, dtype=np.byte)
     
     # Methods for calculating probabilities and expectations over entire distribution
     def expectation(self, f):
@@ -92,17 +104,13 @@ class PopCount:
         """
         return f(self.states).T @ self.p(self.states) 
 
-    def averages(self):
-        """
-        Returns a vector of the expected values <s_i>
-        """
-        return self.expectation(lambda states: states)
-    
-    def correlations(self):
-        """
-        Returns a matrix of the expected values <s_i s_j> where i != j
-        """
-        return np.triu( self.states.T@np.diag(self.p(self.states))@self.states, 1)
+    def prob_k(self):
+            """
+            Returns a vector of the probability of observing K neurons fire
+            """
+            count = np.sum(self.states, axis=1) #how many neurons fire in each states
+            p = self.p(self.states) #probability of each state
+            return np.array([ np.sum(p[ count == i ])  for i in range(self.N + 1)])
 
     def p(self, s):
         """
@@ -118,4 +126,14 @@ class PopCount:
             return -1
             
         return self.p_K[K] / self.combs[K]
+
+    def get_samples(self, M=100000, chains = 4):
+        p_K = np.copy(self.p_K)
+        combs = np.copy(self.combs)
+        @njit
+        def p(s,p_K=p_K):
+            K = int(np.sum(s))
+            return p_K[K] / combs[K] 
+        init_states = np.array([np.random.binomial(1,0.5*np.ones(self.N)) for c in range(chains)])
+        return gibbs_sampling(p,init_states,M,avg_every=self.N,burn_in=self.N*10)
     
